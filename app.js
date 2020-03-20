e9577a88daa3fff9b9cd5fb0577627ae4531bd87
Sonia4 <36730924+Sonia4@users.noreply.github.com>
2020-03-20 17:26:56 +0100

    Probar descarga de archivo

--- app.js
+++ app.js
@@ -20,3 +20,7 @@ app.get('/', function(req,res) {
 app.listen(server_port, server_ip_address, function() {
 	console.log("Listening on " + server_ip_address + ", server_port " + server_port);
 });
+
+app.get('/downloadFile/', (req, res) => {
+  res.download('./test1.txt');
+})
