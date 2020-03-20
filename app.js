95b0e5005d77818dd885f60624f434d4e65bb95d
Sonia4 <36730924+Sonia4@users.noreply.github.com>
2020-03-20 15:47:10 +0100

    Anterior

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 
 // ports setup
-//var server_port = process.env.OPENSHIFT_NODEJS_PORT || 1234;
-//var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 1234;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 // IMPORTANTE: he leído que estas variables ya están obsoletas y Openshift ya no las usa
 // cambiar a ip y puerto en los que sí pueda escuchar la app
 
-var server_port = 8080 || 1234;
-var server_ip_address = '10.129.4.181' || '127.0.0.1';
+//var server_port = 8080 || 1234;
+//var server_ip_address = '10.129.4.181' || '127.0.0.1';
 
 app.get('/', function(req,res) {
 
